8b2d05cb0b75ee755f8db16c93ebe6a15e9b2f00
uzki69 <uzakichan11224@gmail.com>
2026-03-06 21:03:11 -0300

    multiple inputs

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 def main():
 
     p = argparse.ArgumentParser("Links")
-    p.add_argument("input", help="directory located files that will be linked", type=str)
+    p.add_argument("-i", "--input", required=True,  help="directory located files that will be linked", action='append', type=str)
     p.add_argument("output", help="directory where linked files will be", type=str)
     p.add_argument("title", help="tv show title", type=str)
     p.add_argument("season", help="tv show season", type=int)
@@ -14,20 +14,25 @@ def main():
 
     filesToHandle: list[Path] = []
 
-    if type(args.input) is str and type(args.output) is str and type(args.title) is str and type(args.season) is int:
-        for entity in Path(args.input).iterdir(): 
-            if entity.is_file():
-                filesToHandle.append(entity)
+    if type(args.input) is list and type(args.output) is str and type(args.title) is str and type(args.season) is int:
+        # append files to list
+        for input_path in args.input:
+            for entity in Path(input_path).iterdir(): 
+                if entity.is_file():
+                    filesToHandle.append(entity)
+        
+        # create episodes
         episode = 1
         outpath = Path(args.output)
         outpath = Path.joinpath(outpath, f"Season {args.season:02}")
         if not outpath.exists():
             outpath.mkdir(parents=True)
         for file in filesToHandle:
-            handleVideo(file, outpath , args.title, args.season, episode)
+            create_link(file, outpath , args.title, args.season, episode)
             episode+=1
-
-def handleVideo(video: Path, out: Path, title: str, season: int, ep: int):
+    else:
+        print(f'input type error', sys.stderr)
+def create_link(video: Path, out: Path, title: str, season: int, ep: int):
     if not video.name.endswith((".mkv", ".avi", ".mp4", ".webm", ".ts", ".ogg")):
         print(f"video type not supported: {video.name}", file=sys.stderr)
         return
